1fc347767ab9accb5fb7f1abb20f1c4a83b2a948
iceman2299 <xxltri@gmail.com>
2023-12-22 21:54:01 +0300

    not working +

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,6 +1,33 @@
+/*
+//Автоматический режим
 #include <gtest.h>
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
+*/
+
+//Ручной режим
+#pragma once
+#include "TVector.h"
+//#include "TMatrix.h"
+#include <iostream>
+
+int main()//vector
+{
+  setlocale(LC_ALL, "Russian");
+
+
+  TVector<double> first, second, sum, razn;
+  
+  first.Vvod();
+  second.Vvod();
+
+  //sum = first + second;
+  std::cout << "Сумма: " << first+second << "Первый: " << first << "Второй: " << second;
+  razn = first - second;
+  std::cout << "Сумма: " << sum << "Разность: " << razn;
+}
+
+
